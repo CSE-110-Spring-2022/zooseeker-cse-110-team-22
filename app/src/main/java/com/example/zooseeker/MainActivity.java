f7bf4715ff440ff84c9c5c8f64901924e50e582a
Shushruth007 <58798926+Shushruth007@users.noreply.github.com>
2022-05-02 00:12:33 -0700

    load assets

--- app/src/main/java/com/example/zooseeker/MainActivity.java
+++ app/src/main/java/com/example/zooseeker/MainActivity.java
@@ -1,8 +1,15 @@
 package com.example.zooseeker;
 
+import android.os.Bundle;
+import android.util.Log;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import org.jgrapht.Graph;
+import org.jgrapht.GraphPath;
+import org.jgrapht.alg.shortestpath.DijkstraShortestPath;
+
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +17,29 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        String start = "entrance_exit_gate";
+        String goal = "elephant_odyssey";
+
+        // 1. Load the graph...
+        Graph<String, IdentifiedWeightedEdge> g = ZooData.loadZooGraphJSON(this,"sample_zoo_graph.json");
+        GraphPath<String, IdentifiedWeightedEdge> path = DijkstraShortestPath.findPathBetween(g, start, goal);
+
+        // 2. Load the information about our nodes and edges...
+        Map<String, ZooData.VertexInfo> vInfo = ZooData.loadVertexInfoJSON(this,"sample_node_info.json");
+        Map<String, ZooData.EdgeInfo> eInfo = ZooData.loadEdgeInfoJSON(this,"sample_edge_info.json");
+
+        Log.d("Shortest Path",String.format("The shortest path from '%s' to '%s' is:\n", start, goal));
+
+        int i = 1;
+        for (IdentifiedWeightedEdge e : path.getEdgeList()) {
+            Log.d("walked", String.format("  %d. Walk %.0f meters along %s from '%s' to '%s'.\n",
+                    i,
+                    g.getEdgeWeight(e),
+                    eInfo.get(e.getId()).street,
+                    vInfo.get(g.getEdgeSource(e).toString()).name,
+                    vInfo.get(g.getEdgeTarget(e).toString()).name));
+            i++;
+        }
     }
 }
