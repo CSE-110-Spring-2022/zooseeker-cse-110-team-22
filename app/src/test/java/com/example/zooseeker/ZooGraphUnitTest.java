19caa949607eb27d3bc0bad70e3823cfb7de10ec
Shushruth007 <58798926+Shushruth007@users.noreply.github.com>
2022-05-04 23:43:23 -0700

    implemented ZooGraph, ZooGraphUnitTest and Path finding algorithm

--- app/src/test/java/com/example/zooseeker/ZooGraphUnitTest.java
+++ app/src/test/java/com/example/zooseeker/ZooGraphUnitTest.java
@@ -3,6 +3,7 @@ package com.example.zooseeker;
 import static org.junit.Assert.assertNotNull;
 
 import android.content.Context;
+import android.util.Log;
 
 import androidx.test.core.app.ApplicationProvider;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
@@ -11,6 +12,9 @@ import org.jgrapht.GraphPath;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RunWith(AndroidJUnit4.class)
 public class ZooGraphUnitTest {
@@ -41,5 +45,17 @@ public class ZooGraphUnitTest {
         String goal = "elephant_odyssey";
         GraphPath<String, IdentifiedWeightedEdge> path = this.testG.getPath2(start, goal);
         assertNotNull(path);
+    }
+
+    @Test
+    public void testGetShortestPath(){
+        List<String> animal_list = new ArrayList<>();
+        animal_list.add("elephant_odyssey");
+        animal_list.add("gorillas");
+        animal_list.add("gators");
+
+        List<String> output = this.testG.getShortestPath(animal_list);
+        Log.d("out1", output.get(0));
+        assertNotNull(output);
     }
 }
