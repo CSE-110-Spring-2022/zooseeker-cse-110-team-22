69d4664e0a3c33b4c33aa549e06ebff3df486e28
Shushruth007 <58798926+Shushruth007@users.noreply.github.com>
2022-05-07 16:35:43 -0700

    Added Directions page and next button

--- app/src/main/java/com/example/zooseeker/PlanActivity.java
+++ app/src/main/java/com/example/zooseeker/PlanActivity.java
@@ -2,7 +2,9 @@ package com.example.zooseeker;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
@@ -12,18 +14,31 @@ import java.util.stream.Collectors;
 
 public class PlanActivity extends AppCompatActivity {
     ArrayAdapter<String> myAdapter;
+    public static List<String> direction_list;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_plan);
+
+        //Create ListView
         ListView listPlan = ((ListView) findViewById(R.id.listPlan));
+        //Load graph
         ZooGraph zGraph = new ZooGraph(this);
+
+        //Convert list of names to list of ids
         List<String> id_list = MainActivity.planList.stream()
                 .map(name -> MainActivity.nameToId.get(name))
                 .collect(Collectors.toList());
-        List<String> direction_list = zGraph.getShortestPath(id_list);
+
+        //Get list of directions
+        direction_list = zGraph.getShortestPath(id_list);
         myAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, direction_list);
         listPlan.setAdapter(myAdapter);
+    }
+
+    public void openDirections(View view){
+        Intent intent = new Intent(this, DirectionActivity.class);
+        startActivity(intent);
     }
 }
