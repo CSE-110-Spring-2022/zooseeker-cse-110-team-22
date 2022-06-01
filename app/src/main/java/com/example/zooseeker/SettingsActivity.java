decc956e8e823bf903cba0f5f0a747f0bb1ffc0c
Archishma <akavalip@ucsd.edu>
2022-05-31 22:12:54 -0700

    Revert "Updated detailed/brief directions toggle"
    This reverts commit e711984543ffb6dbfcf0044039470af8189c65cc.

--- app/src/main/java/com/example/zooseeker/SettingsActivity.java
+++ app/src/main/java/com/example/zooseeker/SettingsActivity.java
@@ -1,16 +1,11 @@
 package com.example.zooseeker;
 
 import android.content.Context;
-import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.view.MenuItem;
 import android.view.View;
-import android.widget.CompoundButton;
 import android.widget.Switch;
-import android.widget.ToggleButton;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.SwitchCompat;
@@ -25,25 +20,39 @@ public class SettingsActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.settings_activity);
 
+        if (savedInstanceState == null) {
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.settings, new SettingsFragment())
+                    .commit();
+        }
         ActionBar actionBar = getSupportActionBar();
         if (actionBar != null) {
             actionBar.setDisplayHomeAsUpEnabled(true);
         }
     }
 
-    public boolean onOptionsItemSelected(@NonNull MenuItem item){
-        Intent intent = new Intent(this, MainActivity.class);
-        startActivity(intent);
-        return true;
-    }
+    public static class SettingsFragment extends PreferenceFragmentCompat {
+        @Override
+        public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
+            setPreferencesFromResource(R.xml.root_preferences, rootKey);
+            SwitchPreference switchPref = findPreference("settings_switch");
 
-    public void onLaunchSettingsClicked(View view) {
-        Switch settings = findViewById(R.id.settings_switch);
-        if(settings.isChecked()) {
-            checked = true;
+            if (switchPref != null) {
+                switchPref.setOnPreferenceChangeListener((pref, o) -> {
+                    boolean check = (boolean) o;
+                    if (check) {
+                        checked = true;
+                    }
+
+                    else {
+                        checked = false;
+                    }
+                    return true;
+                });
+            }
         }
-        else {
-            checked = false;
-        }
+
+
     }
 }
